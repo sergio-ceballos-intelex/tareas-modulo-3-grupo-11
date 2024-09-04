84267912401123bc5e4fd18a76c2afc2ee24c8cf
William Saldarriaga <wsaldarriagan@gmail.com>
2024-09-04 11:23:01 -0500

    Imprimir numeros que inician en 2

--- WILLIAM_SALDARRIAGA_3175051111/Tarea-3/main.js
+++ WILLIAM_SALDARRIAGA_3175051111/Tarea-3/main.js
@@ -1,4 +1,12 @@
 const limite = 2000;
 for (let i = 1;i<= limite;i++){
-    console.log(`El numero es: ${i}`);
+    const numero = i.toString();
+    
+    // if tradicional
+    // if (numero.startsWith("2")){
+    //     console.log(`El numero es: ${i}`);
+    // }
+
+    // operador condicional -  ternario
+    numero.startsWith("2") ? console.log(`El numero es: ${i}`):0;
 }
