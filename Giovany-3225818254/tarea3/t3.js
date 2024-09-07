ffaf6eb9d017837a9c58be3e1d25e1e550145248
Giovanych <giovaninycarmo12@gmail.com>
2024-09-06 19:58:23 -0500

    modificacion 2

--- Giovany-3225818254/tarea3/t3.js
+++ Giovany-3225818254/tarea3/t3.js
@@ -3,6 +3,14 @@
 // }
 
 
+// for (let i = 1; i <= 2000; i++) {
+//     console.log(i);
+// }
+
+// numbers.js
 for (let i = 1; i <= 2000; i++) {
-    console.log(i);
+    const numStr = i.toString();
+    if (numStr.startsWith('2')) {
+        console.log(i);
+    }
 }
