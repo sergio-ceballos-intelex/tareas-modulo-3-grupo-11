8a9cf9276b4cec14a1d387ca9f3e3a928e4fb20b
William Saldarriaga <wsaldarriagan@gmail.com>
2024-09-09 16:48:42 -0500

    Ajuste if startsWith

--- WILLIAM_SALDARRIAGA_3175051111/Tarea-3/main.js
+++ WILLIAM_SALDARRIAGA_3175051111/Tarea-3/main.js
@@ -2,11 +2,7 @@ const limite = 2000;
 for (let i = 1;i<= limite;i++){
     const numero = i.toString();
     
-    // if tradicional
-    // if (numero.startsWith("2")){
-    //     console.log(`El numero es: ${i}`);
-    // }
-
-    // operador condicional -  ternario
-    numero.startsWith("2") ? console.log(`El numero es: ${i}`):0;
+    if (numero.startsWith("2")){
+        console.log(`El numero es: ${i}`);
+    }
 }
