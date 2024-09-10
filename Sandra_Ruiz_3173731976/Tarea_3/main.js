a7e91599097aa9e020d24d7237ba0411e7657c1e
sanruitap <sanruitap@hotmail.com>
2024-09-10 18:41:39 -0500

    Script muestra los numeros del 1 al 2000

--- Sandra_Ruiz_3173731976/Tarea_3/main.js
+++ Sandra_Ruiz_3173731976/Tarea_3/main.js
@@ -1,5 +1,7 @@
 // tarea 3 
-// punto 1: Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
- for (let i = 1; i <= 50; i++) {
-     console.log(i);
+
+
+//punto 3:Modificar el script para que ahora imprima en consola los numeros del 1 al 2000.
+for (let i = 1; i <= 2000; i++) {
+    console.log(i);
  }
