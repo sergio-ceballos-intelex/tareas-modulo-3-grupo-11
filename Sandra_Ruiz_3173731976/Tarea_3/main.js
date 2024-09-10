6026e62553dc0fa331787c6cb4b82e226426dace
sanruitap <sanruitap@hotmail.com>
2024-09-10 08:27:40 -0500

    Modifique script para que muestre numeros del 1 al 2000

--- Sandra_Ruiz_3173731976/Tarea_3/main.js
+++ Sandra_Ruiz_3173731976/Tarea_3/main.js
@@ -1,6 +1,14 @@
 
 // tarea 3 
 // punto 1: Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
-for (let i = 1; i <= 50; i++) {
+// for (let i = 1; i <= 50; i++) {
+//     console.log(i);
+// }
+//punto 2:Guardar y subir cambios al repositorio remoto con un mensaje que especifique que fue lo que se hizo.
+//ya se supone que hice el local commit y mande el avion con git push y
+//el punto 1.
+//punto 3:Modificar el script para que ahora imprima en consola los numeros del 1 al 2000.
+for (let i = 1; i <= 2000; i++) {
     console.log(i);
 }
+
