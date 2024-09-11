73a803a82f68cca311f3e32d36f5c34c80e5966c
sanruitap <sanruitap@hotmail.com>
2024-09-10 21:32:25 -0500

    consola muestra numeros del 1 al 5

--- Sandra_Ruiz_3173731976/Tarea_3/main.js
+++ Sandra_Ruiz_3173731976/Tarea_3/main.js
@@ -1,9 +1,6 @@
 // tarea 3 
-
-//Punto 5: Modificar el script para que ahora imprima solo los numeros que empiezan por 2 (convertir a string el numero y verificar con starsWith)
-for (let i = 1; i <= 2000; i++) {
-    let numStr = i.toString(); // Se pasa a string
-    if (numStr.startsWith('2')) { // Empieza por 2?
-        console.log(i); // Se le pide que imprima
-    }
-}
+// punto 1: Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
+for (let i = 1; i <= 50; i++) {
+    console.log(i);
+ }
+ 
