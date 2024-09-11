1af939d93f5df40418b7c1884fcb0ac17efbc709
Tu nombre va acá <melissagomezhoyos02@gmail.com>
2024-09-10 21:57:54 -0500

    Modifico la  tarea 3 de numeros del 1 al 2000

--- Melissa_Gomez_3193962490/Tarea_3/Tarea3.js
+++ Melissa_Gomez_3193962490/Tarea_3/Tarea3.js
@@ -1,8 +1,13 @@
-// Script para mostrar los números del 1 al 50 en la consola
+// Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
 
-// Usamos un bucle `for` para iterar desde 1 hasta 50
+// -Usamos un bucle `for` para iterar desde 1 hasta 50
 for (let i = 1; i <= 50; i++) {
     console.log(i);
 }
 
-//B
+//Modificar el script para que ahora imprima en consola los numeros del 1 al 2000.
+
+// -Usamos un bucle `for` para iterar desde 1 hasta 2000
+for (let i = 1; i <= 2000; i++) {
+    console.log(i);
+}
