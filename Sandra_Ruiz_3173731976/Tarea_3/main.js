c3158f26122e8dc25aee9e736384c33fc23b1a44
sanruitap <sanruitap@hotmail.com>
2024-09-10 20:00:46 -0500

    borro tarea 3

--- Sandra_Ruiz_3173731976/Tarea_3/main.js
+++
@@ -1,9 +0,0 @@
-// tarea 3 
-
-//Punto 5: Modificar el script para que ahora imprima solo los numeros que empiezan por 2 (convertir a string el numero y verificar con starsWith)
-for (let i = 1; i <= 2000; i++) {
-    let numStr = i.toString(); // Se pasa a string
-    if (numStr.startsWith('2')) { // Empieza por 2?
-        console.log(i); // Se le pide que imprima
-    }
-}
