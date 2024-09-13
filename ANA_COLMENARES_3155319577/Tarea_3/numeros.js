51388755e0a9a458a312f52f66c238749b9feeac
Ana María Colmenares <anamariacolmenares1@gmail.com>
2024-09-12 20:23:54 -0500

    Script 3 - Tarea 3 - números que empiezan por 2

--- ANA_COLMENARES_3155319577/Tarea_3/numeros.js
+++ ANA_COLMENARES_3155319577/Tarea_3/numeros.js
@@ -1,4 +1,7 @@
-// muestre en consola los numeros del 1 al 2000.
+// muestre en consola solos los números que empiezan en 2 entre el 1 y el 2000
+// convertir a string y verificar con starsWith
 for (let n = 1; n<2001; n++) {
-console.log(n);
+    if (n.toString().startsWith('2')) {
+        console.log(n);
+    }
 }
