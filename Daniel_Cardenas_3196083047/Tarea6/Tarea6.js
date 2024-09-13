2d4b754965e8fb323972111e68104f4506859eb1
Daniel-Cardenas <danielcar_777@hotmail.es>
2024-09-13 17:21:58 -0500

    Elimino commit

---
+++ Daniel_Cardenas_3196083047/Tarea6/Tarea6.js
@@ -0,0 +1,45 @@
+// Primer punto
+let numeros = [1, 2, 3, 4, 5];
+
+const opResult = [];
+
+for (let i = 0; i < numeros.length; i++) {
+    opResult.push(numeros[i] * 2);
+    }
+console.log(opResult);
+
+
+// Punto 2
+// Utiliza un bucle for para encontrar el producto con el precio más bajo. Luego, imprime el nombre y el precio de ese producto.
+
+let productos = [
+    { nombre: "Camisa", precio: 20 },
+    { nombre: "Pantalón", precio: 30 },
+    { nombre: "Zapatos", precio: 50 },
+];
+
+// Punto 3
+// Utiliza un bucle for para incrementar la edad de cada estudiante en 1 año. Luego, imprime el array modificado.
+
+let estudiantes = [
+    { nombre: "Juan", edad: 20 },
+    { nombre: "María", edad: 22 },
+    { nombre: "Pedro", edad: 19 },
+];
+
+
+// Punto 4
+// Utiliza un bucle for para encontrar todos los libros cuyo título contenga la palabra "El". Luego, imprime estos libros.
+
+let libros = [
+    { titulo: "El principito", autor: "Antoine de Saint-Exupéry" },
+    { titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes" },
+    { titulo: "Cien años de soledad", autor: "Gabriel García Márquez" },
+];
+
+// Punto 5
+// Utiliza un bucle for para concatenar frutas2 al final de frutas1. Luego, imprime el array resultante.
+
+let frutas1 = ["Manzana", "Plátano", "Naranja"];
+let frutas2 = ["Fresa", "Uva", "Piña"];
+
