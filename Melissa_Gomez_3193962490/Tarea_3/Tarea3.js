032f9465f5c59006cc5814e532db1d318cae4c74
Melissa <melissagomezhoyos.02@gmail.com>
2024-09-16 19:19:36 -0500

    corrijo tarea 3

--- Melissa_Gomez_3193962490/Tarea_3/Tarea3.js
+++ Melissa_Gomez_3193962490/Tarea_3/Tarea3.js
@@ -1,16 +1,16 @@
 // Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
 
-// // -Usamos un bucle `for` para iterar desde 1 hasta 50
-// for (let i = 1; i <= 50; i++) {
-//     console.log(i);
-// }
+ // -Usamos un bucle `for` para iterar desde 1 hasta 50
+ for (let i = 1; i <= 50; i++) {
+     console.log(i);
+ }
 
 // //Modificar el script para que ahora imprima en consola los numeros del 1 al 2000.
 
-// // -Usamos un bucle `for` para iterar desde 1 hasta 2000
-// for (let i = 1; i <= 2000; i++) {
-//     console.log(i);
-// }
+ // -Usamos un bucle `for` para iterar desde 1 hasta 2000
+ for (let i = 1; i <= 2000; i++) {
+     console.log(i);
+ }
 
 // Modificar el script para que ahora imprima solo los numeros que empiezan por 2 (convertir a string el numero y verificar con starsWith)
 
