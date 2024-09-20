82dc56e6dbe037908800314203de816a95e79f52
Giovanych <giovaninycarmo12@gmail.com>
2024-09-20 11:43:12 -0500

    correccion callback

--- Giovany-3225818254/tarea8/t8.js
+++ Giovany-3225818254/tarea8/t8.js
@@ -1,43 +1,43 @@
-//Devoluciones de llamadas en Manejo de Datos:
+// //Devoluciones de llamadas en Manejo de Datos:
 
-function sumarArreglo(numeros, callback) {
-    const suma = numeros.reduce((total, numero) => total + numero, 0);
-    callback(suma);
-}
+// function sumarArreglo(numeros, callback) {
+//     const suma = numeros.reduce((total, numero) => total + numero, 0);
+//     callback(suma);
+// }
 
-const numeros = [1, 2, 3, 4, 5];
-sumarArreglo(numeros, function (resultado) {
-    console.log("La suma de los números es:", resultado); 
-});
+// const numeros = [1, 2, 3, 4, 5];
+// sumarArreglo(numeros, function (resultado) {
+//     console.log("La suma de los números es:", resultado);
+// });
 
-//Devoluciones de llamada para Filtrar Elementos de un Arreglo:
+// //Devoluciones de llamada para Filtrar Elementos de un Arreglo:
 
-function filtrarArreglo(arreglo, callbackFiltro) {
-    const resultado = arreglo.filter(callbackFiltro);
-    return resultado;
-}
+// function filtrarArreglo(arreglo, callbackFiltro) {
+//     const resultado = arreglo.filter(callbackFiltro);
+//     return resultado;
+// }
 
-const numerosPares = filtrarArreglo(numeros, function (numero) {
-    return numero % 2 === 0;
-});
+// const numerosPares = filtrarArreglo(numeros, function (numero) {
+//     return numero % 2 === 0;
+// });
 
-console.log("Números pares:", numerosPares); 
+// console.log("Números pares:", numerosPares);
 
-//Devoluciones de llamada: Filtrar Números Pares
+// //Devoluciones de llamada: Filtrar Números Pares
 
-function filtrarPares(numeros, callback) {
-    const numerosPares = numeros.filter(numero => numero % 2 === 0);
-    callback(numerosPares);
-}
+// function filtrarPares(numeros, callback) {
+//     const numerosPares = numeros.filter(numero => numero % 2 === 0);
+//     callback(numerosPares);
+// }
 
-function mostrarPares(numerosPares) {
-    console.log("Números pares:", numerosPares);
-}
+// function mostrarPares(numerosPares) {
+//     console.log("Números pares:", numerosPares);
+// }
 
-const numerosEjemplo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-filtrarPares(numerosEjemplo, mostrarPares);
+// const numerosEjemplo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// filtrarPares(numerosEjemplo, mostrarPares);
 
-//Callback asíncrono: Simulación de vuelo desde Colombia hasta Corea del Sur
+// Callback asíncrono: Simulación de vuelo desde Colombia hasta Corea del Sur
 
 function vueloBogotaMadrid() {
     setTimeout(() => {
@@ -52,14 +52,17 @@ function vueloMadridFrankfurt() {
     setTimeout(() => {
         console.log("Vuelo Madrid - Frankfurt, me demoré 2 horas");
         console.log("Llegué a Frankfurt");
-        vueloFrankfurtSeul();
+        vueloFrankfurtSeul(() => {
+            console.log("Secuencia de vuelos completada");
+        });
     }, 2000);
 }
 
-function vueloFrankfurtSeul() {
+function vueloFrankfurtSeul(callback) {
     setTimeout(() => {
         console.log("Vuelo Frankfurt - Seúl, me demoré 10 horas");
         console.log("Llegué a Seúl");
+        callback(); 
     }, 10000);
 }
 
